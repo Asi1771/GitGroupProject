baeedc7fcf5a7ef17aa3c68f01cac2bdf99654a5
kanimarat <kanykeimarat1@gmail.com>
2024-05-02 12:25:33 -0500

    added code

---
+++ src/test/java/tests/SignInTest.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class SignInTest {
+
+    @Test
+    public void signInTest1(){
+        System.out.println("Sign in test");
+    }
+}
